9cc838a23a624fcf444b4ddc57890faecb7c8d20
Crask21 <114492059+Crask21@users.noreply.github.com>
2025-09-25 13:02:41 +0200

    uv package manager and file structure

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from eirt!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/module_template/__init__.py
@@ -0,0 +1,4 @@
+"""Module Template
+This is a template for creating new modules in the project.
+Follow this structure to ensure consistency across modules.
+"""
